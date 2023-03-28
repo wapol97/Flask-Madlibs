9b6458a9a5c65311741bcb6994a7d65219662593
Wade <wadeapollard@gmail.com>
2023-03-28 16:36:27 -0400

    Second commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from stories import story
+from stories import stories
 
 app = Flask(__name__)
 
@@ -13,3 +13,8 @@ def story_result():
     text = story.generate(request.args)
     return render_template('story.html', text=text)
 
+@app.route('/questions')
+def ask_questions():
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+    return render_template('story.html', title=story.title, text=text)
